8a4aa2850fb358f098230556f2f9c643afa1d702
Annette Stellema <40450353+stellema@users.noreply.github.com>
2024-10-04 18:43:36 +1000

    Update CAFE file list and rename dcpp_file_list.py

--- file_lists/CAFE_file_list.py
+++ file_lists/CAFE_file_list.py
@@ -1,20 +1,14 @@
-"""Create CAFE-f6 file lists"""
+"""Create CAFE-f6 file lists."""
 
 import os
 import glob
 
-
+# Precipitation (pr)
 pr_file = "CAFE_c5-d60-pX-f6_pr_files_nov-starts.txt"
 try:
     os.remove(pr_file)
 except OSError:
     pass
-
-# tos_file = "CAFE_c5-d60-pX-f6_tos_files.txt"
-# try:
-#    os.remove(tos_file)
-# except OSError:
-#    pass
 
 infiles_1990s = glob.glob(
     "/g/data/xv83/dcfp/CAFE-f6/c5-d60-pX-f6-199[5,6,7,8,9]11*/atmos_isobaric_daily.zarr.zip"
@@ -28,6 +22,12 @@ with open(pr_file, "a") as outfile:
     for item in infiles_1990s + infiles_2000s:
         outfile.write(f"{item}\n")
 
+# # Oceanic temperature at the surface (tos)
+# tos_file = "CAFE_c5-d60-pX-f6_tos_files.txt"
+# try:
+#    os.remove(tos_file)
+# except OSError:
+#    pass
 # infiles_1990s = glob.glob(
 #    "/g/data/xv83/dcfp/CAFE-f6/c5-d60-pX-f6-199[5,6,7,8,9]*/ocean_month.zarr.zip"
 # )
@@ -40,8 +40,25 @@ with open(pr_file, "a") as outfile:
 #    for item in infiles_1990s + infiles_2000s:
 #        outfile.write(f"{item}\n")
 
+# Daily maximum atmospheric temperature at the surface (tasmax)
+tasmax_file = "CAFE_c5-d60-pX-f6_tasmax_files.txt"
+try:
+    os.remove(tasmax_file)
+except OSError:
+    pass
+infiles_1990s = glob.glob(
+    "/g/data/xv83/dcfp/CAFE-f6/c5-d60-pX-f6-199[5,6,7,8,9]*/atmos_hybrid_daily.zarr.zip"
+)
+infiles_1990s.sort()
+infiles_2000s = glob.glob(
+    "/g/data/xv83/dcfp/CAFE-f6/c5-d60-pX-f6-2???*/atmos_hybrid_daily.zarr.zip"
+)
+infiles_2000s.sort()
+with open(tasmax_file, "a") as outfile:
+    for item in infiles_1990s + infiles_2000s:
+        outfile.write(f"{item}\n")
 
-# Tasmax
+# Tasmax (separate May and Nov starts)
 for m, tasmax_file in zip(
     ["05", "11"],
     [
