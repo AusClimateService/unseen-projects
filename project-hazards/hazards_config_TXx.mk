2d254556c238b29bb58ec807a186b6272ae2d414
Annette Stellema <40450353+stellema@users.noreply.github.com>
2024-11-08 14:28:27 +1100

    Add generic spatial analysis notebook option

--- project-hazards/hazards_config_TXx.mk
+++ project-hazards/hazards_config_TXx.mk
@@ -1,24 +1,41 @@
 # Configuration file for UNSEEN hazards
 
+ENV_DIR=/g/data/xv83/as3189/conda/envs/unseen
 PROJECT_DIR=/g/data/xv83/unseen-projects/outputs/hazards
+FIG_DIR=${PROJECT_DIR}/figures/txx
+## Notebook options
+METRIC_PLOT_LABEL='Temperature [°C]'
+METRIC_PLOT_UPPER_LIMIT=60
+BIAS_CORRECTION=additive
+SIMILARITY_TEST=ks
+TIME_AGG=maximum
+covariate_period=1960-2020 # todo: define period for the covariate here?
+SHAPE_OVERLAP=0.1
+SHAPEFILE=/g/data/xv83/unseen-projects/outputs/hazards/shapefiles/australia.shp
+MIN_LEAD_SHAPE_SPATIAL_AGG=median
+TIME_FREQ=YE-JUN
 
 ## Metric calculation
-
 VAR=tasmax
 UNITS=degC
-
-METRIC_OPTIONS_FCST=--lat_bnds -46 -6 --lon_bnds 110 157 --output_chunks lead_time=50 --reset_times
-METRIC_OPTIONS=--variables ${VAR} --time_freq YE-JUN --time_agg max --input_freq D --time_agg_min_tsteps 360 --time_agg_dates --units tasmax=${UNITS}
+METRIC_OPTIONS_MODEL=--lat_bnds -46 -6 --lon_bnds 110 157 --output_chunks lead_time=50 --reset_times
+METRIC_OPTIONS=--variables ${VAR} --time_freq ${TIME_FREQ} --time_agg max --input_freq D --time_agg_min_tsteps 360 --time_agg_dates --units tasmax=${UNITS}
 
 ## Labels
 METRIC=txx
 REGION=aus
 TIMESCALE=annual-jul-to-jun
-METRIC_PLOT_LABEL='Temperature [°C]'
-METRIC_PLOT_UPPER_LIMIT=60
 
-## Function options
+
+
+## Independence test options
 INDEPENDENCE_OPTIONS=--confidence_interval 0.99 --n_resamples 1000
-MIN_LEAD_OPTIONS=--min_lead_kwargs variables=min_lead shapefile=/g/data/xv83/unseen-projects/outputs/hazards/shapefiles/australia.shp shape_overlap=0.1 spatial_agg=median 
-GEV_STATIONARY_OPTIONS=--fitstart LMM --retry_fit --assert_good_fit --file_kwargs variables=tasmax shapefile=/g/data/xv83/unseen-projects/outputs/hazards/shapefiles/australia.shp shape_overlap=0.1
-GEV_NONSTATIONARY_OPTIONS=--covariate "time.year" --fitstart LMM --retry_fit --pick_best_model 'bic' --file_kwargs variables=tasmax shapefile=/g/data/xv83/unseen-projects/outputs/hazards/shapefiles/australia.shp shape_overlap=0.1
+## Minimum lead time file options (independence file kwargs e.g., median min_lead over shapefile)
+MIN_IND_LEAD_OPTIONS=--min_lead_kwargs variables=min_lead shapefile=${SHAPEFILE} shape_overlap=${SHAPE_OVERLAP} spatial_agg=${MIN_LEAD_SHAPE_SPATIAL_AGG}
+
+## GEV distribution options
+FITSTART=LMM
+GEV_BEST_MODEL_TEST=bic
+GEV_STATIONARY_OPTIONS=--fitstart ${FITSTART} --retry_fit --assert_good_fit --file_kwargs variables=tasmax shapefile=${SHAPEFILE} shape_overlap=0.1
+GEV_NONSTATIONARY_OPTIONS=--covariate "time.year" --fitstart ${FITSTART} --retry_fit --pick_best_model ${GEV_BEST_MODEL_TEST} --file_kwargs variables=tasmax shapefile=${SHAPEFILE} shape_overlap=${SHAPE_OVERLAP}
+
