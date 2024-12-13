5660904ceb2f4ec507ee258a5b9df568fd637679
Annette Stellema <40450353+stellema@users.noreply.github.com>
2024-12-13 15:33:49 +1100

    Add regridded versions of AGCD config files

---
+++ dataset_makefiles/AGCD-CSIRO_r05_tasmax_config.mk
@@ -0,0 +1,8 @@
+# Configuration file for Makefile workflows 
+# AGCD data regridded to 0.05 degree grid
+
+OBS_DATASET=AGCD-CSIRO_r05
+OBS_LABEL=AGCD-CSIRO
+OBS_DATA := /g/data/xv83/unseen-projects/outputs/txx/data/tmax_AGCD-CSIRO_r05_1901-2024.nc
+OBS_CONFIG=/g/data/xv83/unseen-projects/code/dataset_config/dataset_agcd_daily.yml
+OBS_TIME_PERIOD=1901-2024
--- dataset_makefiles/AGCD-tasmax_config.mk
+++ dataset_makefiles/AGCD-tasmax_config.mk
@@ -1,9 +1,6 @@
 # Configuration file for Makefile workflows
 
-OBS_DATASET=AGCD-CSIRO_r05
-# Optional: set the name of the dataset for spatial plot labels
-OBS_LABEL=AGCD
-# OBS_DATA := $(sort $(wildcard /g/data/xv83/agcd-csiro/tmax/daily/tmax_AGCD-CSIRO_r005_*_daily.nc))
-OBS_DATA := /g/data/xv83/unseen-projects/outputs/hazards/data/tmax_AGCD-CSIRO_r05_1901-2024.nc
+OBS_DATASET=AGCD-CSIRO
+OBS_DATA := $(sort $(wildcard /g/data/xv83/agcd-csiro/tmax/daily/tmax_AGCD-CSIRO_r005_*_daily.nc))
 OBS_CONFIG=/g/data/xv83/unseen-projects/code/dataset_config/dataset_agcd_daily.yml
 OBS_TIME_PERIOD=1901-2024
