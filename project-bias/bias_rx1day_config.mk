0d46d7df269355bacfd5746d8ab64c7007892e28
Damien Irving <irving.damien@gmail.com>
2025-05-20 18:17:40 +1000

    Add grid offset

--- project-bias/bias_rx1day_config.mk
+++ project-bias/bias_rx1day_config.mk
@@ -13,7 +13,7 @@ TIMESCALE=annual-jul-to-jun
 VAR=pr
 UNITS=mm day-1
 TIME_FREQ=YE-JUN
-METRIC_OPTIONS=--variables ${VAR} --time_freq ${TIME_FREQ} --time_agg max --input_freq D --time_agg_min_tsteps 360 --time_agg_dates --units ${VAR}='${UNITS}' --regrid_name ${REGION}
+METRIC_OPTIONS=--variables ${VAR} --time_freq ${TIME_FREQ} --time_agg max --input_freq D --time_agg_min_tsteps 360 --time_agg_dates --units ${VAR}='${UNITS}' --regrid_name ${REGION} --regrid_lat_offset 1
 METRIC_OPTIONS_FCST= --output_chunks lead_time=50 --reset_times
 
 
